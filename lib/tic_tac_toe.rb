06fad305f8b3d004a07cbff027b9abaeceb169ee
corytveten <corytveten@gmail.com>
2018-04-16 10:53:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ class TicTacToe
     @board[index] = current_player
   end
 
-  def position_taken?(board, index)
+  def position_taken?(index)
     !(@board[index].nil? || @board[index] == " ")
   end
 end
